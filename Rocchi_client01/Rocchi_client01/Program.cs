2b18d7b32424ac6926ac1e8d03049f75ecae19df
olT1a <101863730+olT1a@users.noreply.github.com>
2022-10-06 15:29:31 +0100

    Add files via upload

---
+++ Rocchi_client01/Rocchi_client01/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+public class SynchronousSocketClient
+{
+
+    public static void StartClient()
+    {
+        byte[] bytes = new byte[1024];
+
+        try
+        {
+            IPAddress ipAddress = System.Net.IPAddress.Parse("127.0.0.1");
+            IPEndPoint remoteEP = new IPEndPoint(ipAddress, 5000);
+            Socket sender = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                sender.Connect(remoteEP);
+                Console.WriteLine("Socket connected to {0}", sender.RemoteEndPoint.ToString());
+                Console.WriteLine("Inserisci un numero...");
+
+                int numero = Convert.ToInt32(Console.ReadLine());
+
+                byte[] msg = Encoding.ASCII.GetBytes(numero.ToString());
+
+                int bytesSent = sender.Send(msg);
+
+                int bytesRec = sender.Receive(bytes);
+                Console.WriteLine("Echoed test = {0}", Encoding.ASCII.GetString(bytes, 0, bytesRec));
+
+                sender.Shutdown(SocketShutdown.Both);
+                sender.Close();
+
+            }
+            catch (ArgumentNullException ane)
+            {
+                Console.WriteLine("ArgumentNullException : {0}", ane.ToString());
+            }
+            catch (SocketException se)
+            {
+                Console.WriteLine("SocketException : {0}", se.ToString());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Unexpected exception : {0}", e.ToString());
+            }
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+    }
+
+    public static int Main(String[] args)
+    {
+        StartClient();
+        Console.WriteLine("Premere un tasto per continuare ");
+        Console.ReadLine();
+        return 0;
+    }
+}
---
+++ Rocchi_server01/Rocchi_server01/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+public class SynchronousSocketListener
+{
+    public static string data = null;
+    public static void StartListening()
+    {
+        byte[] bytes = new Byte[1024];
+        byte[] msg;
+        IPAddress ipAddress = System.Net.IPAddress.Parse("127.0.0.1");
+        IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 5000);
+        Socket listener = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+        try
+        {
+            listener.Bind(localEndPoint);
+            listener.Listen(10);
+
+            while (true)
+            {
+                Console.WriteLine("Waiting for a connection...");
+                Socket handler = listener.Accept();
+                data = null;
+
+                while (true)
+                {
+                    int bytesRec = handler.Receive(bytes);
+                    data += Encoding.ASCII.GetString(bytes, 0, bytesRec);
+                    break;
+                }
+                Random r = new Random();
+                int n = r.Next(1, 10);
+                if (n > Convert.ToInt32(data))
+                {
+                    msg = Encoding.ASCII.GetBytes(n.ToString() + " e' il numero casuale");
+                    handler.Send(msg);
+                }
+                else if (n == Convert.ToInt32(data))
+                {
+                    msg = Encoding.ASCII.GetBytes(n.ToString() + " i numeri sono uguali");
+                    handler.Send(msg);
+                }
+                else
+                {
+                    msg = Encoding.ASCII.GetBytes(data.ToString() + " e' il numero inserito dall'utente");
+                    handler.Send(msg);
+                }
+                handler.Shutdown(SocketShutdown.Both);
+                handler.Close();
+            }
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+
+        Console.WriteLine("\nPress ENTER to continue...");
+        Console.Read();
+
+    }
+
+    public static int Main(String[] args)
+    {
+        StartListening();
+        return 0;
+    }
+}
